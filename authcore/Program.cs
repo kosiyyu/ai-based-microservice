ccc9b249ec1dcce9680e28b0315eacdc9e34417f
Karol Koś <kosiyyu@gmail.com>
2024-03-15 13:23:22 +0100

    Add encryption

--- authcore/Program.cs
+++ authcore/Program.cs
@@ -1,5 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+// database
 await using var ctx = new UserContext();
 await ctx.Database.EnsureDeletedAsync();
 await ctx.Database.EnsureCreatedAsync();
@@ -8,6 +12,12 @@ app.MapGet("/", () => "Hello World!");
 
 app.MapPost("/test", async (User user) => {
   // todo: validate user
+  if(await ctx.Users.AnyAsync(u => u.Email == user.Email)){
+    return "User already exists";
+  }
+
+  user.Password = Aes.Encrypt(user.Password);
+
   ctx.Users.Add(user);
   await ctx.SaveChangesAsync();
   return "User added";
