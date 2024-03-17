4669f4f080b908b532d07d26b4be6fd3973d4e27
Karol Koś <kosiyyu@gmail.com>
2024-03-17 01:25:51 +0100

    Add login

--- vue-ui/src/App.vue
+++ vue-ui/src/App.vue
@@ -1,47 +1,52 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="bg-white py-2 px-1">
+    <div class="flex items-center justify-between bg-white rounded-lg py-2 px-2">
+      <div class="text-white">Logo</div>
+      <div class="flex items-center">
+        <button class="bg-white h-8 hover:bg-special text-black font-bold py-1 px-4 rounded-full ml-2">
+          Log in
+        </button>
+        <button class="bg-white h-8 hover:bg-special text-black font-bold py-1 px-4 rounded-full ml-2">
+          Register
+        </button>
+        <div class="ml-2 h-8 w-8">
+          <a href="https://github.com/kosiyyu/ai-based-microservice">
+            <img src="./assets/github-mark.svg"/>
+          </a>
+        </div>
+      </div>
     </div>
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="bg-green-950 flex flex-col items-center h-screen">
+  <div class="flex flex-grow items-center justify-center flex-col">
+    <div class="font-special text-special-pink text-5xl mb-4">
+      Application name
+    </div>
+    <div class="flex items-center justify-center">
+      <img src="./assets/pixeltrue-vision-1.svg" class="w-72 h-72"/>
+      <div class="bg-black border border-black flex flex-col items-center justify-center p-6 inline-block rounded-tr-lg rounded-br-lg">
+        <div class="m-2 flex flex-col">
+          <label class="text-special-pink font-bold mb-2">Email</label>
+          <input class="bg-special-pink  text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-special-pink"/>
+        </div>
+        <div class="p-2 flex flex-col">
+          <label class="text-special-pink font-bold mb-2">Password</label>
+          <input class="bg-special-pink  text-black rounded-lg focus:outline-none focus:ring-2 focus:ring-special-pink"/>
+        </div>
+        <div class="flex items-center justify-center p-2">
+          <button class="bg-special-pink hover:bg-special-pink text-black font-bold py-1 px-4 rounded-full">
+            Log in
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+    <footer class="bg-black">
+      footer
+    </footer>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
---
+++ vue-ui/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'special': 'rgba(19, 247, 46, 0.8)',
+        'special-pink': 'rgba(254, 164, 211, 0.8)',
+      },
+      fontFamily: {
+        'special': ['Abril Fatface', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
