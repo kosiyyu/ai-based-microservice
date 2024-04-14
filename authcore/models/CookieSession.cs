a94815a06548998904c264872ee3b1ad5d80c237
Karol Koś <kosiyyu@gmail.com>
2024-04-14 20:18:39 +0200

    Remove old services, cleanup

--- authcore/models/CookieSession.cs
+++
@@ -1,7 +0,0 @@
-public class CookieSession
-{
-  public string Id { get; set; }
-  public int UserId { get; set; }
-  public DateTime ExpirationDate { get; set; }
-  public DateTime CreationDate { get; set; }
-}
--- authcore/models/CookieSessionDto.cs
+++
@@ -1,6 +0,0 @@
-public class CookieSessionDto
-{
-  public int UserId { get; set; }
-  public DateTime ExpirationDate { get; set; }  
-  public DateTime CreationDate { get; set; }
-}
