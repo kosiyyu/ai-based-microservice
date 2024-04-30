ad512a8a86d5ea905838cc4217e90fc9b82206f8
Karol Koś <kosiyyu@gmail.com>
2024-04-30 13:40:11 +0200

    Update WebSocket server

--- ollama-bridge/index.ts
+++ ollama-bridge/index.ts
@@ -1,10 +1,12 @@
 import { WebSocket } from "ws";
 import { Ollama } from "ollama-node";
 
+const modelName = "dolphin-phi";
+
 const ollama = new Ollama();
-ollama.setModel("dolphin-phi")
+ollama.setModel(modelName)
   .then(() => {
-    console.log("Model loaded");
+    console.log(modelName);
   }).catch((err) => {
     console.log(err);
   });
@@ -15,11 +17,14 @@ const wsServer = new WebSocket.Server({ port: 3333 }, () => {
 
 wsServer.on("connection", socket => {
   console.log("connected")
-
-  socket.on("message", function(msg) {
+  socket.on("message", function (msg) {
     console.log('received: %s', msg);
-      ollama.streamingGenerate(`${msg}`, (output: string) => {
-        socket.send(output);
-      });
+    if (!msg || `${msg}` === "") {
+      return;
+    }
+    console.log("pass");
+    ollama.streamingGenerate(`${msg}`, (output: string) => {
+      socket.send(output);
+    });
   });
 });
